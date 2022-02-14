1fd8e84ddf659c46e15446faef01f107aa725cff
CassianoCardoso <dj_kassiano@hotmail.com>
2022-02-13 21:03:51 -0300

    etapa 1

---
+++ Salario  Funcionario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Salario__Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu numero");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o numero de horas trabalhada");
+            int numHoras = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor que recebe por horas");
+            double valorHoras = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double sal = valorHoras * numHoras;
+
+            Console.WriteLine("O seu numero é " + num + "E o seu salario é " + sal.ToString("F2", CultureInfo.InvariantCulture)); ;
+
+            Console.ReadLine();
+        }
+    }
+}
